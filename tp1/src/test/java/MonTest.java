95cd429e39b0de1db25b9051023460a18d618a45
lucas-raingeard <lucas.raingeard09@gmail.com>
2026-09-10 11:34:53 +0200

    ajout commentaire

--- tp1/src/test/java/MonTest.java
+++ tp1/src/test/java/MonTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import tp1.Add;
  
+// Classe test pour la fonction Add
 public class MonTest {
  
   @Test
   public void simpleTest() {
+    // Test unitaire de l'addition de deux entier
     Assertions.assertEquals(3, Add.add(1, 2));
   }
 
